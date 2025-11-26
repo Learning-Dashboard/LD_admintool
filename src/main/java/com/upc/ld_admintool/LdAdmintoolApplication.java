eea150699c57e91c22c7a68cc5d1664d0fb33f0b
geerardferrer <gerard.ferrer.oriol@estudiantat.upc.edu>
2025-11-26 20:50:06 +0100

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/upc/ld_admintool/LdAdmintoolApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ld_admintool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdAdmintoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdAdmintoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/ld_admintool/LdAdmintoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.ld_admintool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdAdmintoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
