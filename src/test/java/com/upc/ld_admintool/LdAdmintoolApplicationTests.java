71f9444b32fd5696a8fbda30e4ad8998d7970d59
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2026-05-20 12:37:20 +0200

    feat: added apikey

--- src/test/java/com/upc/ld_admintool/LdAdmintoolApplicationTests.java
+++ src/test/java/com/upc/ld_admintool/LdAdmintoolApplicationTests.java
@@ -3,7 +3,7 @@ package com.upc.ld_admintool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "LD_API_KEY=test-ld-api-key")
 class LdAdmintoolApplicationTests {
 
 	@Test
